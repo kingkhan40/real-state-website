71f110e3d91b9001816691abcf2e6ebf6dbda7c4
King Khan <uk2079239@gmail.com>
2024-06-01 00:22:06 -0700

    Frontend And Backend Setup

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,40 @@
+import { FaSearch } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <header className='bg-slate-100 shadow-xl'>
+      <div className='flex justify-between items-center max-w-7xl mx-auto p-3'>
+        <Link to='/'>
+          <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+            <span className='text-slate-500 text-xl '>Khan</span>
+          </h1>
+        </Link>
+        <form className='bg-slate-200 px-5 py-2 rounded-full flex items-center'>
+          <input
+            type='text'
+            placeholder='Search...'
+            className='bg-transparent focus:outline-none w-24 sm:w-64'
+          />
+          <FaSearch className='text-slate-600 cursor-pointer' />
+        </form>
+        <ul className='flex gap-6'>
+          <Link to='/'>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>
+              Home
+            </li>
+          </Link>
+          <Link to='/about'>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>
+              About
+            </li>
+          </Link>
+          <Link to='/sign-in'>
+            <li className=' text-slate-700 hover:underline'> Sign in</li>
+          </Link>
+       
+        </ul>
+      </div>
+    </header>
+  );
+}
