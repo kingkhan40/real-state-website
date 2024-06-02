e4d07d3020b5b65d73757a650dfe935a22991331
King Khan <uk2079239@gmail.com>
2024-06-01 20:10:37 -0700

    Create Signin Routes And UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,29 +2,25 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
-import Signin from './pages/Signin';
-import Signup from './pages/Signup';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import Header from './components/Header';
 
+
 const App = () => {
   return (
-    <>
-
-      <BrowserRouter>
+    <BrowserRouter>
       <Header />
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="sign-in" element={<Signin />} />
-          <Route path="sign-up" element={<Signup />} />
-          <Route path="profile" element={<Profile />} />
-
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
